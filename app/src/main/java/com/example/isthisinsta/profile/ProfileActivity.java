615caac5ac2e06d8cb015db2dc8551909df3574d
Kshitiz <kshitizyadavofficial@gmail.com>
2022-07-24 00:46:55 +0530

    Created profile section layout

--- app/src/main/java/com/example/isthisinsta/profile/ProfileActivity.java
+++ app/src/main/java/com/example/isthisinsta/profile/ProfileActivity.java
@@ -2,8 +2,11 @@ package com.example.isthisinsta.profile;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.isthisinsta.BottomNavigationMenu;
 import com.example.isthisinsta.R;
@@ -18,14 +21,33 @@ public class ProfileActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: called");
         super.onCreate(savedInstanceState);
         this.overridePendingTransition(0, 0);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
 
+        setToolBar();
         setBottomNavBar();
-
     }
 
     private void setBottomNavBar() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavBar);
         BottomNavigationMenu.enableNavigation(this, bottomNavigationView, ACTIVITY_NUM);
+    }
+
+    private void setToolBar(){
+        Toolbar toolbar = findViewById(R.id.topProfileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_page_menu, menu);
+        return true;
     }
 }
